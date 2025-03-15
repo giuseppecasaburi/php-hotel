ab441502c460d21de094063664c6bcc65413a026
giuseppecasaburi <enzobiondi0000@libero.it>
2025-03-15 16:30:45 +0100

    first commit

---
+++ homePage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bool B&B</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Bool B&B ti da il benvenuto</h1>
+        <h3>Scegli i requisiti che il tuo Hotel perfetto dovrebbe rispettare <br> o semplicemente avvia la ricerca!</h3>
+        <div class="container-form">
+            <form action="./resultPage.php" method="GET">
+                <label for="voto_recensioni">Scegli il voto minimo assegnato alla struttura che cerchi</label>
+                <select name="voto_recensioni">
+                    <option value="inserisci_voto">Inserisci un voto minimo</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+
+                <label for="parcheggio">Parcheggio:</label>
+                <select name="parcheggio" id="">
+                    <option value="all" checked>Tutti
+                    <option value="true">Con parcheggio
+                    <option value="false">Senza parcheggio
+                </select>
+
+                <button type="submit">Avvia la ricerca</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resultPage.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bool B&B Ricerca</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+    ?>
+
+    <?php
+    echo "<h1 class='result-title'>Eccola il nostro catalogo di hotel scelti apposta per te!</h1>";
+
+    $parcheggio = $_GET["parcheggio"];
+    $voto = $_GET["voto_recensioni"];
+    $array_filtrato = [];
+
+    foreach ($hotels as $hotel) {
+        if (($voto === "inserisci_voto" || $hotel["vote"] >= $voto) && ($parcheggio === "all" || $hotel["parking"] === ($parcheggio === "true"))) {
+            $array_filtrato[] = $hotel;
+        };
+    };
+    ?>
+
+    <div>
+        <table class="table table-striped ">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($array_filtrato as $hotel_filtrati) { ?>
+                    <tr>
+                        <td><?php echo $hotel_filtrati["name"] ?></td>
+                        <td><?php echo $hotel_filtrati["description"] ?></td>
+                        <td><?php echo $hotel_filtrati["vote"] ?></td>
+                        <td><?php echo $hotel_filtrati["parking"] ? "Si" : "No" ?></td>
+                        <td><?php echo $hotel_filtrati["distance_to_center"] ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
